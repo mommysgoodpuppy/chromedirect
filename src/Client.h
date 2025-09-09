d53b3c7d236d50ee794019fa9dd68d9176723488
mommysgoodpuppy <theelectronicfreezer@gmail.com>
2025-09-09 18:08:28 +0300

    clean

--- src/Client.h
+++ src/Client.h
@@ -6,7 +6,6 @@
 #include <include/cef_life_span_handler.h>
 #include <include/cef_display_handler.h>
 #include <include/cef_request_handler.h>
-#include <include/cef_load_handler.h>
 #include <windows.h>
 #include <memory>
 #include <atomic>
@@ -16,9 +15,7 @@ class OffscreenClient final : public CefClient,
                               public CefLifeSpanHandler,
                               public CefRenderHandler,
                               public CefDisplayHandler,
-                              public CefRequestHandler,
-                              public CefLoadHandler,
-                              public CefResourceRequestHandler {
+                              public CefRequestHandler {
 public:
   OffscreenClient(HWND host_window, std::shared_ptr<Presenter> presenter, int width, int height, float scale = 1.0f, int frame_rate = 60);
 
@@ -27,19 +24,11 @@ public:
   CefRefPtr<CefRenderHandler> GetRenderHandler() override { return this; }
   CefRefPtr<CefDisplayHandler> GetDisplayHandler() override { return this; }
   CefRefPtr<CefRequestHandler> GetRequestHandler() override { return this; }
-  CefRefPtr<CefLoadHandler> GetLoadHandler() override { return this; }
   bool OnProcessMessageReceived(CefRefPtr<CefBrowser> browser,
                                 CefRefPtr<CefFrame> frame,
                                 CefProcessId source_process,
                                 CefRefPtr<CefProcessMessage> message) override;
-  CefRefPtr<CefResourceRequestHandler> GetResourceRequestHandler(
-      CefRefPtr<CefBrowser> browser,
-      CefRefPtr<CefFrame> frame,
-      CefRefPtr<CefRequest> request,
-      bool is_navigation,
-      bool is_download,
-      const CefString& request_initiator,
-      bool& disable_default_handling) override { return this; }
+  
 
   // CefLifeSpanHandler
   void OnAfterCreated(CefRefPtr<CefBrowser> browser) override;
@@ -73,39 +62,6 @@ public:
                       CefRefPtr<CefRequest> request,
                       bool user_gesture,
                       bool is_redirect) override;
-  cef_return_value_t OnBeforeResourceLoad(CefRefPtr<CefBrowser> browser,
-                                          CefRefPtr<CefFrame> frame,
-                                          CefRefPtr<CefRequest> request,
-                                          CefRefPtr<CefCallback> callback) override;
-  // Resource completion diagnostics
-  void OnResourceLoadComplete(CefRefPtr<CefBrowser> browser,
-                              CefRefPtr<CefFrame> frame,
-                              CefRefPtr<CefRequest> request,
-                              CefRefPtr<CefResponse> response,
-                              CefResourceRequestHandler::URLRequestStatus status,
-                              int64_t received_content_length) override;
-  CefRefPtr<CefResponseFilter> GetResourceResponseFilter(
-      CefRefPtr<CefBrowser> browser,
-      CefRefPtr<CefFrame> frame,
-      CefRefPtr<CefRequest> request,
-      CefRefPtr<CefResponse> response) override;
-
-  // Load diagnostics
-  void OnLoadingStateChange(CefRefPtr<CefBrowser> browser,
-                            bool isLoading,
-                            bool canGoBack,
-                            bool canGoForward) override;
-  void OnLoadStart(CefRefPtr<CefBrowser> browser,
-                   CefRefPtr<CefFrame> frame,
-                   CefLoadHandler::TransitionType transition_type) override;
-  void OnLoadEnd(CefRefPtr<CefBrowser> browser,
-                 CefRefPtr<CefFrame> frame,
-                 int httpStatusCode) override;
-  void OnLoadError(CefRefPtr<CefBrowser> browser,
-                   CefRefPtr<CefFrame> frame,
-                   cef_errorcode_t errorCode,
-                   const CefString& errorText,
-                   const CefString& failedUrl) override;
 
   HWND GetHostHwnd() const { return host_window_; }
   CefRefPtr<CefBrowser> GetBrowser() const { return browser_; }
